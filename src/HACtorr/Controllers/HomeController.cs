885b74d1a553bc82406cba2cdbe25cfe0bdfa7c6
hussainkali <hussaink.ali@gmail.com>
2012-08-08 00:14:44 +0100

    Clean Project

---
+++ src/HACtorr/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Hussain
+ * Date: 07/08/2012
+ * Time: 22:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Web.Mvc;
+
+namespace HACtorr.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+		
+		public ActionResult Contact()
+		{
+			return View();
+		}
+	}
+}
